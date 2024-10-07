1c2c47774e694ade041e42f655bd0f068bc358d8
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2024-10-07 11:59:01 +0200

    Cut down the code abit

--- src/list/list.rs
+++ src/list/list.rs
@@ -12,20 +12,9 @@ pub fn list_available_passwords(file_path: PathBuf) {
         if let Ok(line) = line {
             if line.contains("password_name") {
                 password_count += 1;
-                let line_split: Result<String, &str> = line
-                    .split(" ")
-                    .last()
-                    .ok_or("No words found")
-                    .and_then(|s| s.parse::<String>().map_err(|_| "Failed to parse string"));
-
-                match line_split {
-                    Ok(password) => {
-                        println!("{}. {}", password_count, password);
-                    }
-                    Err(e) => {
-                        println!("Error: {}", e);
-                    }
-                }
+                if let Some(password) = line.split(" ").last() {
+                    println!("{}. {}", password_count, password);
+                };
             }
         }
     }
