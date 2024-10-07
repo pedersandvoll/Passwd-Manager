88299c69b60a6d008cf670417ad4d0a5ce44452e
Peder Sandvoll <97838211+pedersandvoll@users.noreply.github.com>
2024-10-07 09:16:10 +0200

    Moved generate password into a shared loop so if you cancel after creating passwordname it will not be saved to file

--- src/generate/generate_password.rs
+++ src/generate/generate_password.rs
@@ -26,49 +26,56 @@ pub fn generate_password(file_path: PathBuf) {
     let mut password_value = String::new();
 
     loop {
-        println!("Enter name of password you want to create:");
-        let _ = stdout().flush();
-        stdin()
-            .read_line(&mut password_name)
-            .expect("Failed try again");
+        loop {
+            println!("Enter name of password you want to create:");
+            let _ = stdout().flush();
+            stdin()
+                .read_line(&mut password_name)
+                .expect("Failed try again");
+
+            let formatted_name = format!("password_name {}", password_name.trim());
+
+            if file_content.contains(&formatted_name) {
+                println!("Name cannot be the same as another saved password name");
+                password_name.clear();
+            } else {
+                break;
+            }
+        }
+
+        loop {
+            println!("Enter password for {}", password_name.trim());
+            let _ = stdout().flush();
+            stdin()
+                .read_line(&mut password_value)
+                .expect("Failed try again");
+
+            if password_value.trim().len() < 8 {
+                println!("Password cannot be less than 8 characters");
+                password_value.clear();
+            } else {
+                break;
+            }
+        }
 
         let formatted_name = format!("password_name {}", password_name.trim());
 
-        if file_content.contains(&formatted_name) {
-            println!("Name cannot be the same as another saved password name");
-            password_name.clear();
-        } else {
-            let _ = file
-                .write_all(formatted_name.as_bytes())
-                .expect("Name append failed");
-            file.write_all(b"\n").expect("Failed to write newline");
-            file.flush().expect("Failed to flush after name append");
-            break;
-        }
-    }
+        let _ = file
+            .write_all(formatted_name.as_bytes())
+            .expect("Name append failed");
+        file.write_all(b"\n").expect("Failed to write newline");
+        file.flush().expect("Failed to flush after name append");
 
-    loop {
-        println!("Enter password for {}", password_name.trim());
-        let _ = stdout().flush();
-        stdin()
-            .read_line(&mut password_value)
-            .expect("Failed try again");
-
-        if password_value.trim().len() < 8 {
-            println!("Password cannot be less than 8 characters");
-            password_value.clear();
-        } else {
-            let formatted_password = format!(
-                "password({}) {}",
-                password_name.trim(),
-                password_value.trim()
-            );
-            let _ = file
-                .write_all(formatted_password.as_bytes())
-                .expect("Password append failed");
-            file.write_all(b"\n").expect("Failed to write newline");
-            file.flush().expect("Failed to flush after password append");
-            break;
-        }
+        let formatted_password = format!(
+            "password({}) {}",
+            password_name.trim(),
+            password_value.trim()
+        );
+        let _ = file
+            .write_all(formatted_password.as_bytes())
+            .expect("Password append failed");
+        file.write_all(b"\n").expect("Failed to write newline");
+        file.flush().expect("Failed to flush after password append");
+        break;
     }
 }
